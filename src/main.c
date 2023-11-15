10d1777e6264147764a38c6242dda069b81587c8
Chris Garrett <chris@chrisg.com>
2023-11-15 17:23:45 +0000

    output scancodes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "SDL.h"
 #include <stdio.h>
 
+// A basic game object
 typedef struct GameObjects {
   int x, y;
   int width;
@@ -9,74 +10,102 @@ typedef struct GameObjects {
   char *name;
 } GameObject;
 
+// This is where we will move our characters
 int processEvents(SDL_Window *window, GameObject *player)
 {
+
+  // Alternative to checking keydown event
+  const Uint8 *state = SDL_GetKeyboardState(NULL);
+
+  // To store the event
   SDL_Event event;
+
+  // Variable for if we should quit
   int done = 0;
 
+  // Check for SDL events
   while(SDL_PollEvent(&event))
   {
+
+    // Figure out which type of event it is
     switch(event.type)
     {
+
+      // They closed the window - how rude!
       case SDL_WINDOWEVENT_CLOSE:
       {
+        // Is the window open?
         if(window)
         {
+          // Guess we are finished, close up!
           SDL_DestroyWindow(window);
           window = NULL;
           done = 1;
         }
-      }
-      break;
+      } break;
+
+      // Escape Key
       case SDL_KEYDOWN:
       {
-        switch(event.key.keysym.sym)
+
+        // Check the keyboard scancode
+        switch(event.key.keysym.scancode)
         {
-          case SDLK_ESCAPE:
+
+          // Tell the game we are done
+          case SDL_SCANCODE_ESCAPE:
             done = 1;
-          break;
+            break;
+
+          // Cursor scancodes
+          case SDL_SCANCODE_LEFT:
+            player->x -= 10;
+            break;
+
+          case SDL_SCANCODE_RIGHT:
+            player->x += 10;
+            break;
+
+          case SDL_SCANCODE_UP:
+            player->y -= 10;
+            break;
+
+          case SDL_SCANCODE_DOWN:
+            player->y += 10;
+            break;
+
+          default:
+            printf("%d",event.key.keysym.scancode);
+            fflush(stdout);
+            break;
+
         }
-      }
-      break;
+      } break;
+
+      // End of our Main fires this event
       case SDL_QUIT:
         //quit out of the game
         done = 1;
       break;
     }
   }
-  
-  const Uint8 *state = SDL_GetKeyboardState(NULL);
-  if(state[SDL_SCANCODE_LEFT])
-  {
-    player->x -= 10;
-  }
-  if(state[SDL_SCANCODE_RIGHT])
-  {
-    player->x += 10;
-  }
-  if(state[SDL_SCANCODE_UP])
-  {
-    player->y -= 10;
-  }
-  if(state[SDL_SCANCODE_DOWN])
-  {
-    player->y += 10;
-  }
-  
+
   return done;
 }
 
 // Render the graphics to the screen
 void doRender(SDL_Renderer *renderer, GameObject *this_game_object)
 {
-  //set the drawing color to black
+  // Set the drawing color to black
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   
-  //Clear the screen
+  // Clear the view area
   SDL_RenderClear(renderer);
   
-  //set the drawing color to red
+  // Set the drawing color to red
   SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+  // An exciting red box
   SDL_Rect rect = { this_game_object->x, this_game_object->y, this_game_object->width, this_game_object->height };
   SDL_RenderFillRect(renderer, &rect);
   
@@ -105,11 +134,14 @@ int main(int argc, char *argv[])
   int window_height;
   SDL_GetWindowSizeInPixels(window,&window_width,&window_height);
 
+
+  // Set up the player object and start centered
   GameObject player;
-  player.x = window_width/2;
-  player.y = window_height/2;
   player.width = 32;
   player.height = 32;
+  player.x = (window_width/2)-(player.width/2);
+  player.y = (window_height/2)-(player.height/2);
+
 
   // The window is open: enter program loop (see SDL_PollEvent)
   int done = 0;
@@ -124,7 +156,7 @@ int main(int argc, char *argv[])
     doRender(renderer, &player);
     
     //don't burn up the CPU
-    SDL_Delay(10);
+    SDL_Delay(5);
   }
   
   
